adb8ba57c2d15cba718ed06dc6244cd774ec0004
gfed53 <gfederico7153@gmail.com>
2017-07-17 11:39:37 -0400

    Removed unnecessary firebase config

--- src/firebase.config.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	var config = {
-		apiKey: 'AIzaSyBUOByHyjpFQRxG_paFSdhA95QcKx9XMsI',
-		authDomain: 'burning-torch-898.firebaseapp.com',
-		databaseURL: 'https://burning-torch-898.firebaseio.com/',
-		storageBucket: 'burning-torch-898.appspot.com'
-	};
-	firebase.initializeApp(config);
-})();
